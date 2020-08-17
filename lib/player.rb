78bacae8d0d1787441ea483c9d890ee93c176c85
Shaun James <ShaunDaneJames@Gmail.com>
2020-08-17 16:37:26 -0600

    Adds has lost method and passes test

--- lib/player.rb
+++ lib/player.rb
@@ -1,8 +1,14 @@
 class Player
+  attr_reader :name,
+              :deck
 
   def initialize(name, deck)
     @name = name
     @deck = deck
-  end 
+  end
+
+  def has_lost?
+    @deck.cards.count == 0
+  end
 
 end
--- test/player_test.rb
+++ test/player_test.rb
@@ -6,7 +6,7 @@ require './lib/player'
 
 class PlayerTest < Minitest::Test
 
-  def test_it_exists_with_attributes
+  def test_it_exists_with_a_deck
     card1 = Card.new(:diamond, 'Queen', 12)
     card2 = Card.new(:spade, '3', 3)
     card3 = Card.new(:heart, 'Ace', 14)
@@ -15,6 +15,24 @@ class PlayerTest < Minitest::Test
     player = Player.new('Calvin', deck)
 
     assert_instance_of Player, player
+    assert_equal "Calvin", player.name
+    assert_equal deck, player.deck
   end
 
+  def test_knows_who_lost
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+    player = Player.new('Calvin', deck)
+
+    refute player.has_lost?
+    player.deck.remove_card
+    refute player.has_lost?
+    player.deck.remove_card
+    refute player.has_lost?
+    player.deck.remove_card
+    assert player.has_lost?
+  end
 end
