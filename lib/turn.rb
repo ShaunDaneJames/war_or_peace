d9d5169493f01a115f565fd163e2c26ec5a892c0
Shaun James <ShaunDaneJames@Gmail.com>
2020-08-18 22:38:04 -0600

    Adds all winner outcomes and passes all tests

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,12 +1,14 @@
 class Turn
   attr_reader :player1,
               :player2,
-              :spoils_of_war
+              :spoils_of_war,
+              :players
 
   def initialize(player1, player2)
     @player1 = player1
     @player2 = player2
     @spoils_of_war = []
+    @players = [@player1, @player2]
   end
 
   def type
@@ -21,6 +23,16 @@ class Turn
     elsif @player1.deck.rank_of_card_at(0) ==
       @player2.deck.rank_of_card_at(0)
       turn = :war
+    end
+  end
+
+  def winner
+    if type == :mutually_assured_destruction
+      "No Winner"
+    elsif type == :basic
+      @players.max_by { |player| player.deck.rank_of_card_at(0) }
+    elsif type == :war
+      @players.max_by { |player| player.deck.rank_of_card_at(2) }
     end
   end
 
--- test/turn_test.rb
+++ test/turn_test.rb
@@ -60,7 +60,6 @@ class TurnTest < Minitest::Test
     turn = Turn.new(player1, player2)
 
     assert_equal :basic, turn.type
-
   end
 
   def test_it_knows_turn_type_war
@@ -79,7 +78,6 @@ class TurnTest < Minitest::Test
     turn = Turn.new(player1, player2)
 
     assert_equal :war, turn.type
-
   end
 
   def test_it_knows_turn_type_mutually_assured_destruction
@@ -88,7 +86,7 @@ class TurnTest < Minitest::Test
     card3 = Card.new(:heart, '9', 9)
     card4 = Card.new(:diamond, 'Jack', 11)
     card5 = Card.new(:heart, '8', 8)
-    card6 = Card.new(:diamond, '8', 8) 
+    card6 = Card.new(:diamond, '8', 8)
     card7 = Card.new(:heart, '3', 3)
     card8 = Card.new(:diamond, '2', 2)
     deck1 = Deck.new([card1, card2, card5, card8])
@@ -98,6 +96,59 @@ class TurnTest < Minitest::Test
     turn = Turn.new(player1, player2)
 
     assert_equal :mutually_assured_destruction, turn.type
+  end
 
+  def test_it_knows_winner_of_basic_turn
+    card1 = Card.new(:heart, 'Jack', 11)
+    card2 = Card.new(:heart, '10', 10)
+    card3 = Card.new(:heart, '9', 9)
+    card4 = Card.new(:diamond, 'Jack', 11)
+    card5 = Card.new(:heart, '8', 8)
+    card6 = Card.new(:diamond, 'Queen', 12)
+    card7 = Card.new(:heart, '3', 3)
+    card8 = Card.new(:diamond, '2', 2)
+    deck1 = Deck.new([card1, card2, card5, card8])
+    deck2 = Deck.new([card3, card4, card6, card7])
+    player1 = Player.new("Calvin", deck1)
+    player2 = Player.new("Hobbes", deck2)
+    turn = Turn.new(player1, player2)
+
+    assert_equal player1, turn.winner
+  end
+
+  def test_it_knows_winner_of_war_turn
+    card1 = Card.new(:heart, 'Jack', 11)
+    card2 = Card.new(:heart, '10', 10)
+    card3 = Card.new(:heart, '9', 9)
+    card4 = Card.new(:diamond, 'Jack', 11)
+    card5 = Card.new(:heart, '8', 8)
+    card6 = Card.new(:diamond, 'Queen', 12)
+    card7 = Card.new(:heart, '3', 3)
+    card8 = Card.new(:diamond, '2', 2)
+    deck1 = Deck.new([card1, card2, card5, card8])
+    deck2 = Deck.new([card4, card3, card6, card7])
+    player1 = Player.new("Calvin", deck1)
+    player2 = Player.new("Hobbes", deck2)
+    turn = Turn.new(player1, player2)
+
+    assert_equal player2, turn.winner
+  end
+
+  def test_it_knows_MAD_has_no_winner
+    card1 = Card.new(:heart, 'Jack', 11)
+    card2 = Card.new(:heart, '10', 10)
+    card3 = Card.new(:heart, '9', 9)
+    card4 = Card.new(:diamond, 'Jack', 11)
+    card5 = Card.new(:heart, '8', 8)
+    card6 = Card.new(:diamond, '8', 8)
+    card7 = Card.new(:heart, '3', 3)
+    card8 = Card.new(:diamond, '2', 2)
+    deck1 = Deck.new([card1, card2, card5, card8])
+    deck2 = Deck.new([card4, card3, card6, card7])
+    player1 = Player.new("Calvin", deck1)
+    player2 = Player.new("Hobbes", deck2)
+    turn = Turn.new(player1, player2)
+
+    assert_equal "No Winner", turn.winner
   end
 end
