e0d6b25cde6717499226cf1f69b4a83769a32088
Shaun James <ShaunDaneJames@Gmail.com>
2020-08-18 21:43:58 -0600

    Adds all turn types and passes all test for each turn type

--- lib/turn.rb
+++ lib/turn.rb
@@ -10,17 +10,17 @@ class Turn
   end
 
   def type
-    if @player1.deck.rank_of_card_at(0) !=
-      @player2.deck.rank_of_card_at(0)
-      turn = :basic
-    elsif @player1.deck.rank_of_card_at(0) ==
-      @player2.deck.rank_of_card_at(0)
-      turn = :war
-    elsif (@player1.deck.rank_of_card_at(0) ==
+    if (@player1.deck.rank_of_card_at(0) ==
       @player2.deck.rank_of_card_at(0)) &&
       (@player1.deck.rank_of_card_at(2) ==
       @player2.deck.rank_of_card_at(2))
       turn = :mutually_assured_destruction
+    elsif @player1.deck.rank_of_card_at(0) !=
+    @player2.deck.rank_of_card_at(0)
+    turn = :basic
+    elsif @player1.deck.rank_of_card_at(0) ==
+      @player2.deck.rank_of_card_at(0)
+      turn = :war
     end
   end
 
--- test/turn_test.rb
+++ test/turn_test.rb
@@ -44,7 +44,7 @@ class TurnTest < Minitest::Test
     assert_equal [], turn.spoils_of_war
   end
 
-  def test_it_knows_turn_type
+  def test_it_knows_turn_type_basic
     card1 = Card.new(:heart, 'Jack', 11)
     card2 = Card.new(:heart, '10', 10)
     card3 = Card.new(:heart, '9', 9)
@@ -60,5 +60,44 @@ class TurnTest < Minitest::Test
     turn = Turn.new(player1, player2)
 
     assert_equal :basic, turn.type
+
+  end
+
+  def test_it_knows_turn_type_war
+    card1 = Card.new(:heart, 'Jack', 11)
+    card2 = Card.new(:heart, '10', 10)
+    card3 = Card.new(:heart, '9', 9)
+    card4 = Card.new(:diamond, 'Jack', 11)
+    card5 = Card.new(:heart, '8', 8)
+    card6 = Card.new(:diamond, 'Queen', 12)
+    card7 = Card.new(:heart, '3', 3)
+    card8 = Card.new(:diamond, '2', 2)
+    deck1 = Deck.new([card1, card2, card5, card8])
+    deck2 = Deck.new([card4, card3, card6, card7])
+    player1 = Player.new("Calvin", deck1)
+    player2 = Player.new("Hobbes", deck2)
+    turn = Turn.new(player1, player2)
+
+    assert_equal :war, turn.type
+
+  end
+
+  def test_it_knows_turn_type_mutually_assured_destruction
+    card1 = Card.new(:heart, 'Jack', 11)
+    card2 = Card.new(:heart, '10', 10)
+    card3 = Card.new(:heart, '9', 9)
+    card4 = Card.new(:diamond, 'Jack', 11)
+    card5 = Card.new(:heart, '8', 8)
+    card6 = Card.new(:diamond, '8', 8) 
+    card7 = Card.new(:heart, '3', 3)
+    card8 = Card.new(:diamond, '2', 2)
+    deck1 = Deck.new([card1, card2, card5, card8])
+    deck2 = Deck.new([card4, card3, card6, card7])
+    player1 = Player.new("Calvin", deck1)
+    player2 = Player.new("Hobbes", deck2)
+    turn = Turn.new(player1, player2)
+
+    assert_equal :mutually_assured_destruction, turn.type
+
   end
 end
